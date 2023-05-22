661e61308328f420de9913d80a9f32b5a6568158
reycesawyer <reycesawyer@yahoo.com>
2023-05-22 22:44:20 +0000

    input and print combo lab

---
+++ challenge_lab13.py
@@ -0,0 +1,5 @@
+name = input("What is your name? >> ")
+day = input("What is the day of the week? >> ")
+temp = input("What is the weather like today? >> ")
+
+print("Hello,", name + "! Today is", day + "! It is", temp + " out right now!")
---
+++ challenge_lab13_2.py
@@ -0,0 +1,5 @@
+name = input("What is your name? >> ")
+day = input("What is the day of the week? >> ")
+
+print(f"Hello, {name}! Today is {day}.")
+
